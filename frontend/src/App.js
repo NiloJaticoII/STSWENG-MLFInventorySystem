7ae9be01aa95226ec59cbd241368ceaa32a2ae42
TilapiaRoger <jessamyn_lapan@dlsu.edu.ph>
2020-12-08 01:56:58 +0800

    Adapted the login page for ReactJS

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div id="loginPage" class="App">
+        <div id="loginBox" class="container">
+            <form id="loginForm" method="POST" action="/login">
+                <div class="text-center">
+                    <img id="logoImage" src="photo/logo.png" /> 
+                </div>
+
+                <div class="form-group">
+                    <label for="userName" class="font-weight-normal">username</label>
+                    <input id="userName" name="userName" type="text" class="form-control" />
+                </div>
+                <div class="form-group">
+                    <label for="userName" class="font-weight-normal">password</label>
+                    <input id="password" name="password" type="password" class="form-control" />
+                </div>
+                <div class="text-right">
+                    <input id="loginButton" value="LOGIN" type="submit" class="btn btn-secondary btn-lg col-5" />
+                </div>
+            </form>
+        </div>
     </div>
-  );
+    );
 }
 
 export default App;
