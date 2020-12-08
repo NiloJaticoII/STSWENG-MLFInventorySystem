2b10079617383352d845ed9eacffe283bc6aa17b
Ruhneko <nicholas_custodio@dlsu.edu.ph>
2020-12-08 14:16:46 +0800

    jest react test trial

---
+++ frontend/src/__tests__/App.test.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import { render, screen, shallow } from '@testing-library/react';
+
+//Enzyme tests matchers
+import { configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+configure({ adapter: new Adapter() });
+
+// jest-dom adds custom jest matchers for asserting on DOM nodes. expect(element).toHaveTextContent(/react/i)
+import '@testing-library/jest-dom';
+
+import App from '../App';
+
+test('renders login page', () => {
+  render(<App />);
+  const linkElement = screen.getByText("username");
+  expect(linkElement).toBeInTheDocument();
+});
