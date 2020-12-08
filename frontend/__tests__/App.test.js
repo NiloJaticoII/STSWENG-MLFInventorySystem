9ed83561dc8fcef193affd07c7c18e3f39bbdd5b
Ruhneko <nicholas_custodio@dlsu.edu.ph>
2020-12-08 14:19:07 +0800

    jest test update

--- frontend/__tests__/App.test.js
+++ frontend/__tests__/App.test.js
@@ -9,7 +9,7 @@ configure({ adapter: new Adapter() });
 // jest-dom adds custom jest matchers for asserting on DOM nodes. expect(element).toHaveTextContent(/react/i)
 import '@testing-library/jest-dom';
 
-import App from '../App';
+import App from '../src/App';
 
 test('renders login page', () => {
   render(<App />);
