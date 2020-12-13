996e6698fdcbb5338ef161838313d4fa49587412
James Kevin Lin <lin.james0825@yahoo.com>
2020-12-13 16:56:31 +0800

    add MainMenu.test.js

--- frontend/__tests__/MainMenu.test.js
+++ frontend/__tests__/MainMenu.test.js
@@ -9,7 +9,7 @@ configure({ adapter: new Adapter() });
 // jest-dom adds custom jest matchers for asserting on DOM nodes. expect(element).toHaveTextContent(/react/i)
 import '@testing-library/jest-dom';
 
-import MainMenu from '../src/ComponentsHome/MainMenu';
+import MainMenu from '../src/Components/Home/MainMenu';
 
 test('renders MainMenu page', () => {
   render(<MainMenu />);
